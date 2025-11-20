6aadf8456783696e6647ec1c4896c557ec59ce30
marianoariassoy <hola@marianoarias.soy>
2025-11-20 18:14:29 -0300

    chore: build

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -8,12 +8,12 @@ export const menu = [
     title_en: 'Plants',
     title_es: 'Plantas',
     url: '/plants'
+  },
+  {
+    title_en: 'Journal',
+    title_es: 'Entradas',
+    url: '/journal'
   }
-  // {
-  //   title_en: 'Journal',
-  //   title_es: 'Entradas',
-  //   url: '/journal'
-  // },
   // {
   //   title_en: 'Texts',
   //   title_es: 'Textos',
